4b3960bd14f68198e034bc363d1aed7782f21de6
Priyanshu <psingh1002@gmail.com>
2019-12-26 18:45:05 +0530

    Added entity

--- src/main/java/com/priyanshu/fifa/FifaRatingsApplication.java
+++ src/main/java/com/priyanshu/fifa/FifaRatingsApplication.java
@@ -1,13 +1,45 @@
 package com.priyanshu.fifa;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Entity
 public class FifaRatingsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FifaRatingsApplication.class, args);
 	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double rating;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getRating() {
+		return rating;
+	}
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+	
 
 }
