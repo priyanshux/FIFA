286642a2feb2be65984bed379253767e18356a71
Priyanshu <psingh1002@gmail.com>
2019-12-26 19:03:24 +0530

    data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO FifaRatingsApplication(name,rating) VALUES('Lionel Messi', 98.5);
+INSERT INTO FifaRatingsApplication(name,rating) VALUES('Cristiano Ronaldo', 97.5);
+INSERT INTO FifaRatingsApplication(name,rating) VALUES('Sadio Mane', 96.0);
+INSERT INTO FifaRatingsApplication(name,rating) VALUES('Neymar Jr', 95.5);
+INSERT INTO FifaRatingsApplication(name,rating) VALUES('Kylian Mbappe', 94.5);
+INSERT INTO FifaRatingsApplication(name,rating) VALUES('Robert Lewandowski', 92.5);
