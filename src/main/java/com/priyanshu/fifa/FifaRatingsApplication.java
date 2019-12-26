6fda7c63a62bffb81a89ffdd42fc6457280839c0
Priyanshu <psingh1002@gmail.com>
2019-12-26 18:29:11 +0530

    adding base

---
+++ src/main/java/com/priyanshu/fifa/FifaRatingsApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshu.fifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaRatingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaRatingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshu/fifa/FifaRatingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.fifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaRatingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
