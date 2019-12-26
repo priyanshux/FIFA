70842e23463e548d8c1f9493f9a449e221b4b451
Priyanshu <psingh1002@gmail.com>
2019-12-26 19:03:57 +0530

    edit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,5 @@ INSERT INTO FifaRatingsApplication(name,rating) VALUES('Sadio Mane', 96.0);
 INSERT INTO FifaRatingsApplication(name,rating) VALUES('Neymar Jr', 95.5);
 INSERT INTO FifaRatingsApplication(name,rating) VALUES('Kylian Mbappe', 94.5);
 INSERT INTO FifaRatingsApplication(name,rating) VALUES('Robert Lewandowski', 92.5);
+
+COMMIT;
